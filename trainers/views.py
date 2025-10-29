aca55b96a816a0804eb8d160ca4e2b22a6cbdb5a
Ayman-Feteha <fetehaayman@gmail.com>
2025-10-29 13:12:43 +0300

    allow login by email also

--- trainers/views.py
+++ trainers/views.py
@@ -1,3 +1,54 @@
 from django.shortcuts import render
-
+from .serializers import TrainerSerializer
+from .models import Trainer
+from rest_framework.views import APIView
+from rest_framework.response import Response
 # Create your views here.
+
+class TrainerView(APIView):
+    
+    def get(self, request):
+        trainers = Trainer.objects.all()
+        serializer = TrainerSerializer(trainers, many=True)
+        return Response(serializer.data)
+    
+    def post(self, request):
+        serializer = TrainerSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=201)
+        return Response(serializer.errors, status=400)
+    
+class TrainerUpdateView(APIView):
+    def put(self, request, trainer_id):
+        try:
+            trainer = Trainer.objects.get(id=trainer_id)
+        except Trainer.DoesNotExist:
+            return Response({"error": "Trainer not found"}, status=404)
+        
+        serializer = TrainerSerializer(trainer, data=request.data, partial=True)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=400)
+    
+    def delete(self, request, trainer_id):
+        try:
+            trainer = Trainer.objects.get(id=trainer_id)
+        except Trainer.DoesNotExist:
+            return Response({"error": "Trainer not found"}, status=404)
+        
+        trainer.delete()
+        return Response(status=204)
+    
+    def patch(self, request, trainer_id):
+        try:
+            trainer = Trainer.objects.get(id=trainer_id)
+        except Trainer.DoesNotExist:
+            return Response({"error": "Trainer not found"}, status=404)
+        
+        serializer = TrainerSerializer(trainer, data=request.data, partial=True)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=400)
