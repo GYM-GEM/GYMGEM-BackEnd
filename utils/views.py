d891971e3127d36cedf5225e1dc7948e21c4120c
Alsayedaldahawy <alsayedaldahawy@outlook.com>
2025-11-30 23:52:25 +0200

    Fixed: Token issue & tested

--- utils/views.py
+++ utils/views.py
@@ -3,6 +3,7 @@ from rest_framework import serializers
 from GymGem import settings
 from accounts.models import Account
 
+
 # Create your views here.
 def get_account_from_token(request):
     auth_header = request.headers.get("Authorization")
@@ -14,11 +15,7 @@ def get_account_from_token(request):
         if token:
 
             try:
-                payload = jwt.decode(
-                    token,
-                    settings.SECRET_KEY,
-                    algorithms=["HS256"]
-                )
+                payload = jwt.decode(token, settings.SECRET_KEY, algorithms=["HS256"])
             except Exception as e:
                 print("Token decode error:", str(e))
 
@@ -29,7 +26,7 @@ def get_account_from_token(request):
     print("Account ID from token:", account_id)
     if not account_id:
         raise serializers.ValidationError("Account ID not found in token.")
-    
+
     try:
         return Account.objects.get(pk=account_id)
     except Account.DoesNotExist:
@@ -45,14 +42,12 @@ def get_profile_id_from_token(request):
     if auth_header and auth_header.startswith("Bearer "):
         token = auth_header.split(" ")[1]
     payload = None
+
+    print("TTTTTTT:", token)
     if token:
 
         try:
-            payload = jwt.decode(
-                token,
-                settings.SECRET_KEY,
-                algorithms=["HS256"]
-            )
+            payload = jwt.decode(token, settings.SECRET_KEY, algorithms=["HS256"])
         except Exception as e:
             print("Token decode error:", str(e))
     profile_id = payload.get("current_profile", None) if payload else None
