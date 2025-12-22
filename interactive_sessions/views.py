8752ec5e704252d89a07ba5599776c3e417e34f6
unknown <fetehaayman@gmail.com>
2025-12-22 14:20:37 +0200

    list sessions

--- interactive_sessions/views.py
+++ interactive_sessions/views.py
@@ -352,6 +352,6 @@ class SessionListView(APIView):
                 )
             serializer = InteractiveSessionSerializer(sessions, many=True, context={'request': request})
             data = serializer.data.exclude('scheduled_at','created_at','updated_at')
-            return Response({**data,"role": role}, status=200)
+            return Response({"data": data,"role": role}, status=200)
         except Exception as e:
             return Response({'error': str(e)}, status=400)
