7216947b87dedd067be2e318e4a67ccfe2973637
Alsayedaldahawy <alsayedaldahawy@outlook.com>
2025-12-22 20:16:39 +0200

    add frontend deployed link to CORS_ALLOWED_ORIGINS

--- GymGem/settings.py
+++ GymGem/settings.py
@@ -105,6 +105,7 @@ ROOT_URLCONF = "GymGem.urls"
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:4040",
     "http://127.0.0.1:4040",
+    "https://gymgem-backend.onrender.com"
 ]
 CORS_ALLOW_CREDENTIALS = True
 CORS_ALLOW_HEADERS = list(default_headers) + [
