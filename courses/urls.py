8cbf7e861c958ce60f4363e5b6ac0ae3e0fe4aa5
Ayman-Feteha <fetehaayman@gmail.com>
2025-12-26 15:05:34 +0200

    dashboard apis

--- courses/urls.py
+++ courses/urls.py
@@ -1,6 +1,6 @@
 from rest_framework.routers import DefaultRouter
-from .views import CourseProgressView, CoursesView, LessonsView, LessonSectionsView, CourseEnrollmentsView
-
+from .views import CourseProgressView, CoursesView, LessonsView, LessonSectionsView, CourseEnrollmentsView,CourseAdminView
+from django.urls import path
 router = DefaultRouter()
 router.register(r'courses', CoursesView, basename='courses')
 router.register(r'lessons', LessonsView, basename='lessons')
@@ -9,3 +9,7 @@ router.register(r'enrollments', CourseEnrollmentsView, basename='enrollments')
 router.register(r'progress', CourseProgressView, basename='progress')
 
 urlpatterns = router.urls
+
+urlpatterns += [
+    path('admin/', CourseAdminView.as_view(), name='course-admin'),
+]
