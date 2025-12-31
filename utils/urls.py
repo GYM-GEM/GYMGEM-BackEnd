aa5a782891a93d8a0dbc09abb369e3964dc2121d
Ayman-Feteha <fetehaayman@gmail.com>
2025-12-31 20:22:36 +0200

    complaint

--- utils/urls.py
+++ utils/urls.py
@@ -1,7 +1,11 @@
 from django.urls import path
-from .views import CategoryListView, SpecializationListView
+from .views import CategoryListView, SpecializationListView, SendComplaint, ComplaintStatusView, ComplaintUpdateView
 
 urlpatterns = [
     path("categories/", CategoryListView.as_view(), name="categories-list"),
     path("specializations/", SpecializationListView.as_view(), name="specializations-list"),
+    path("complaints/", SendComplaint.as_view(), name="complaint-create"),
+    path("complaints/<int:complaint_id>/", ComplaintStatusView.as_view(), name="complaint-status"),
+    path("complaints/<int:complaint_id>/update/", ComplaintUpdateView.as_view(), name="complaint-update"),
+    
 ]
