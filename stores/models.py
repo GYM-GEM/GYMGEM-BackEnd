9f7e64ca5c433d82d74c699df66276b6fe331193
Alsayedaldahawy <alsayedaldahawy@outlook.com>
2026-01-01 09:52:56 +0200

    add product image

--- stores/models.py
+++ stores/models.py
@@ -87,7 +87,8 @@ class StoreItem(models.Model):
     branch_id = models.ForeignKey(StoreBranch, on_delete=models.CASCADE, blank=True, null=True)
     name = models.CharField(max_length=100)
     description = models.CharField(max_length=255, blank=True, null=True)
-    price = models.DecimalField(max_digits=10, decimal_places=2)
+    item_image = models.URLField(blank=True, null=True)
+    price = models.IntegerField(default=0, validators=[MinValueValidator(0)])
     category = models.CharField(max_length=100, choices=[('supplements', 'Supplements'), ('clothes', 'Clothes'),('foods', 'Foods')], blank=True, null=True)
     brand = models.CharField(max_length=100, blank=True, null=True)
     expiration_date = models.DateField(blank=True, null=True)
@@ -119,6 +120,7 @@ class StoreItemInventory(models.Model):
     quantity = models.PositiveIntegerField()
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+    
 
     def __str__(self):
         return f"StoreItemInventory<{self.store_item_id.name} - {self.size_id.name if self.size_id else 'No Size'}>"
