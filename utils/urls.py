ea1d8da9d35de7cc870584013ea39abb8a719b91
Ayman-Feteha <fetehaayman@gmail.com>
2026-01-04 15:01:54 +0200

    msgs done

--- utils/urls.py
+++ utils/urls.py
@@ -5,7 +5,7 @@ urlpatterns = [
     path("categories/", CategoryListView.as_view(), name="categories-list"),
     path("specializations/", SpecializationListView.as_view(), name="specializations-list"),
     path("complaints/", SendComplaint.as_view(), name="complaint-create"),
-    path("complaints/<int:complaint_id>/", ComplaintStatusView.as_view(), name="complaint-status"),
+    path("complaints/", ComplaintStatusView.as_view(), name="complaint-status"),
     path("complaints/<int:complaint_id>/update/", ComplaintUpdateView.as_view(), name="complaint-update"),
     path("admin/complaints/", CompaintAdminListView.as_view(), name="admin-complaint-list"),
 ]
