95bcaa788236a615e172d534ba672f7eadb7d8a9
Alsayedaldahawy <alsayedaldahawy@outlook.com>
2026-01-04 15:54:47 +0200

    allowed hosts config

--- GymGem/settings.py
+++ GymGem/settings.py
@@ -199,11 +199,7 @@ MIDDLEWARE = [
 ROOT_URLCONF = "GymGem.urls"
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:4040",
-    "http://127.0.0.1:4040",
-    "https://gymgem-theta.vercel.app",
-    "http://192.168.1.7:4040",
-    "http://192.168.1.2:4040",
-    "http://192.168.1.5:4040",
+    "http://192.168.1.20:4040",
 ]
 CORS_ALLOW_CREDENTIALS = True
 CORS_ALLOW_HEADERS = list(default_headers) + [
